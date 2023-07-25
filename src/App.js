da685869eec0ec376dd1e6d06ece0fbd67b3e489
Subhasini Gupta <guptasubhasini68@gmail.com>
2023-07-25 19:29:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import React , {useState , useEffect} from 'react';
 
 function App() {
+  const [data,  setData] =useState(0);
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +19,10 @@ function App() {
         >
           Learn React
         </a>
+        <h3>{data}</h3>
+        <button onClick={() => setData(data+1)}>Add me</button>
+        <button onClick={() => setData(data-1)}>Subtract me</button>
+      
       </header>
     </div>
   );
